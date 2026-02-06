df7f6ff3d34a6bd21dd9b1cec187cff7169ff531
Uumutunal <unal.umut98@gmail.com>
2026-02-06 11:09:31 +0300

    github config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,15 +20,15 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://uumutunal.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/CosmoNode_Manual/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Uumutunal', // Usually your GitHub org/user name.
+  projectName: 'CosmoNode_Manual', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
@@ -92,9 +92,6 @@ const config = {
 
 
       footer: {
-        colorMode: {
-          respectPrefersColorScheme: true,
-        },
         links: [
 /*           {
             title: 'Docs',
